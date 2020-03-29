6c94b4803722c7f786381c0599b6698b61e05e5f
oswystan <oswystan@126.com>
2020-03-29 11:50:21 +0800

    feature: add rfc.sh for downloading rfc files

---
+++ scripts/work/rfc.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+###########################################################################
+##                     Copyright (C) 2020 wystan
+##
+##       filename: rfc.sh
+##    description:
+##        created: 2020-03-29 11:36:56
+##         author: wystan
+##
+###########################################################################
+
+rfc() {
+    local one=""
+    if [[ $# -le 0 ]]; then
+        echo "usage: $FUNCNAME <rfc-number> ..."
+        return 1
+    fi
+    for one in $*
+    do
+        one="rfc${one}.txt"
+        printf "%-18s: " $one && \
+        wget https://www.rfc-editor.org/rfc/$one -O $one > /dev/null 2>&1 && \
+        echo "succ" || echo "fail"
+    done
+}
+
+###########################################################################
