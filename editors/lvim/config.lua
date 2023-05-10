acf0079e08d7c257b902e0e7f282d43eb74e6188
oswystan <oswystan@126.com>
2023-05-10 21:20:12 +0800

    refactor: add new plugins to lvim

--- editors/lvim/config.lua
+++ editors/lvim/config.lua
@@ -21,4 +21,23 @@ vim.cmd[[au BufReadPost * if line("'\"") > 0|if line("'\"") <= line("$")|exe("no
 lvim.leader = ","
 lvim.keys.normal_mode["<leader>z"] = ":qa!<CR>"
 lvim.keys.normal_mode["<leader>x"] = ":wa!<CR>"
+lvim.keys.normal_mode["H"]         = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["L"]         = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<leader>t"] = ":TagbarToggle<CR>"
+lvim.keys.normal_mode["<leader>w"] = ":NERDTreeToggle<CR>"
+lvim.keys.visual_mode["<leader>e"] = ":Tabularize /=<CR>"
+lvim.keys.visual_mode["<leader>c"] = ":Tabularize /:<CR>"
 
+
+----------------------------------
+-- plugins
+----------------------------------
+vim.g.tagbar_left = 1
+vim.g.tagbar_width = 30
+lvim.plugins = {
+    {"scrooloose/nerdcommenter"},
+    {"scrooloose/nerdtree"},
+    {"majutsushi/tagbar"},
+    {"godlygeek/tabular"},
+    {"vim-scripts/a.vim"}
+}
