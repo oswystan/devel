7003484345e029f07145b79dc4631df46303816a
wystan <oswystan@126.com>
2023-06-29 11:10:26 +0800

    feature: add formatter in nvchad

--- editors/nvchad/lua/custom/configs/null-ls.lua
+++ editors/nvchad/lua/custom/configs/null-ls.lua
@@ -10,16 +10,22 @@ local sources = {
 
   -- webdev stuff
   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  b.formatting.prettier.with({ filetypes = { "html", "markdown", "css" } }), -- so prettier works only on these filetypes
 
   -- Lua
   b.formatting.stylua,
 
   -- cpp
   b.formatting.clang_format,
+
+  -- golang
+  b.formatting.gofmt,
+
+  -- shell
+  b.formatting.shfmt,
 }
 
-null_ls.setup {
+null_ls.setup({
   debug = true,
   sources = sources,
-}
+})
--- editors/nvchad/lua/custom/init.lua
+++ editors/nvchad/lua/custom/init.lua
@@ -8,31 +8,31 @@
 --
 -------------------------------------------------------------------------------
 local opt = vim.opt
-local g   = vim.g
+local g = vim.g
 local cmd = vim.cmd
+local autocmd = vim.api.nvim_create_autocmd
 
 --------------------
 -- base options
 --------------------
-
-opt.mouse       = nil
-opt.tabstop     = 4
+opt.mouse = nil
+opt.tabstop = 4
 opt.softtabstop = 4
-opt.shiftwidth  = 4
-opt.expandtab   = true
-opt.ignorecase  = true
-opt.guicursor   = "a:block"
-opt.whichwrap   = "<,>"
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.ignorecase = true
+opt.guicursor = "a:block"
+opt.whichwrap = "<,>"
 
 g.mapleader = ","
 
-g.vscode_snippets_path = vim.fn.stdpath "config" .. "/lua/custom/snippets"
+g.vscode_snippets_path = vim.fn.stdpath("config") .. "/lua/custom/snippets"
 
 -- goto last postion
-cmd [[au BufReadPost * if line("'\"") > 0|if line("'\"") <= line("$")|exe("norm '\"")|else|exe "norm $"|endif|endif]]
+cmd([[au BufReadPost * if line("'\"") > 0|if line("'\"") <= line("$")|exe("norm '\"")|else|exe "norm $"|endif|endif]])
 
 -- toggle quickfix window borrowed from lvim
-cmd [[
+cmd([[
   function! QuickFixToggle()
     if empty(filter(getwininfo(), 'v:val.quickfix'))
       copen
@@ -40,6 +40,17 @@ cmd [[
       cclose
     endif
   endfunction
-]]
+]])
+
+autocmd("FileType", {
+  pattern = { "c", "cpp", "lua", "sh", "go" },
+  callback = function()
+    autocmd("BufWritePre", {
+      callback = function()
+        vim.lsp.buf.format()
+      end,
+    })
+  end,
+})
 
 -------------------------------------------------------------------------------
