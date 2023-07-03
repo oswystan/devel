d5bf21b00897a3845b59c6e2ce83a31db4bcfb28
wystan <oswystan@126.com>
2023-07-03 10:00:37 +0800

    refactor: add copy mode

--- editors/nvchad/lua/custom/mappings.lua
+++ editors/nvchad/lua/custom/mappings.lua
@@ -10,17 +10,24 @@
 
 local M = {}
 
+Copymode = function()
+  vim.cmd([[set nonu]])
+  vim.cmd([[IndentBlanklineDisable]])
+  vim.cmd([[set signcolumn=no]])
+end
+
 M.general = {
-    n = {
-        [";"] = { ":", "enter command mode", opts = { nowait = true } },
-        ["<leader>z"] = { "<cmd> q! <CR>", "quit current buffer" },
-        ["<leader>t"] = { "<cmd> TagbarToggle <CR>", "toggle tagbar" },
-        ["<leader>,"] = { "<cmd> call QuickFixToggle() <CR>", "toggle quickfix window" },
-    },
-    v = {
-        ["<C-=>"] = {":Tabularize /=<CR>", opts = {remap = true}},
-        ["<C-;>"] = {":Tabularize /:<CR>", opts = {remap = true}},
-    }
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>z"] = { "<cmd> q! <CR>", "quit current buffer" },
+    ["<leader>t"] = { "<cmd> TagbarToggle <CR>", "toggle tagbar" },
+    ["<leader>,"] = { "<cmd> call QuickFixToggle() <CR>", "toggle quickfix window" },
+    ["<leader>p"] = { "<cmd> lua Copymode() <CR>", "set copy mode" },
+  },
+  v = {
+    ["<C-=>"] = { ":Tabularize /=<CR>", opts = { remap = true } },
+    ["<C-;>"] = { ":Tabularize /:<CR>", opts = { remap = true } },
+  },
 }
 
 M.tabufline = {
@@ -39,7 +46,7 @@ M.tabufline = {
         require("nvchad_ui.tabufline").tabuflinePrev()
       end,
       "Goto prev buffer",
-    }
+    },
   },
 }
 
