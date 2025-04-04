ecb64f1527d48704c670c91d1d1661a44888b274
oswystan <oswystan@126.com>
2025-04-04 17:25:37 +0800

    add ip.sh

---
+++ scripts/env/ip.sh
@@ -0,0 +1,23 @@
+###############################################################################
+##
+##       filename: ip.sh
+##    description: ip utils
+##        created: 2025/04/04
+##         author: wystan
+##
+###############################################################################
+
+function ip.lan() {
+  ifconfig | grep 'inet ' | awk '{print $2}'|grep -v "127.0.0"
+}
+
+function ip.wan() {
+  curl -s ifconfig.me
+  echo ""
+}
+
+function ip.location() {
+  curl -s http://myip.ipip.net
+}
+
+###############################################################################
