464b612a7aabad38abf6665491171a78d5767e72
oswystan <oswystan@126.com>
2025-04-17 18:50:37 +0800

    add ssl.sh for show cert related file content

---
+++ scripts/env/ssl.sh
@@ -0,0 +1,15 @@
+###############################################################################
+##
+##       filename: ssl.sh
+##    description:
+##        created: 2025/04/17
+##         author: ticktechman
+##
+###############################################################################
+
+alias crt.cat='openssl x509 -noout -text -in '
+alias cert.cat='openssl x509 -noout -text -in '
+alias csr.cat='openssl req -noout -text -in '
+alias key.rsa.cat='openssl rsa -noout -text -in '
+
+###############################################################################
