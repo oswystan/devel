54ddfdf42776c911205e7dcad29e18c87529ffb8
oswystan <oswystan@126.com>
2025-04-17 20:16:37 +0800

    key.ec.cat

--- scripts/env/ssl.sh
+++ scripts/env/ssl.sh
@@ -11,5 +11,6 @@ alias crt.cat='openssl x509 -noout -text -in '
 alias cert.cat='openssl x509 -noout -text -in '
 alias csr.cat='openssl req -noout -text -in '
 alias key.rsa.cat='openssl rsa -noout -text -in '
+alias key.ec.cat='openssl ec -noout -text -in '
 
 ###############################################################################
