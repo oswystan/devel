7eb0aeea2d08d2480954f39121f8af702a10b2dd
oswystan <oswystan@126.com>
2025-04-18 19:04:23 +0800

    styling

--- scripts/env/ssl.sh
+++ scripts/env/ssl.sh
@@ -17,38 +17,52 @@ alias ssl.ciphers='openssl ciphers -v 'ALL:@SECLEVEL=1''
 function ssl.cert.RSA-SHA256() {
   [[ $# -ne 1 ]] && { echo "usage: $0 <domain-name>"; return 1; }
   local CN="$1"
-  openssl req -x509 -newkey rsa -sha256 -keyout server.key -out server.crt -days 365 -nodes -subj "/CN=${CN}"
+  openssl req -x509 \
+    -newkey rsa \
+    -sha256 \
+    -keyout server.key \
+    -out server.crt \
+    -days 365 \
+    -nodes \
+    -subj "/CN=${CN}"
 }
 function ssl.cert.RSA-SHA384() {
   [[ $# -ne 1 ]] && { echo "usage: $0 <domain-name>"; return 1; }
   local CN="$1"
-  openssl req -x509 -newkey rsa -sha384 -keyout server.key -out server.crt -days 365 -nodes -subj "/CN=${CN}"
+  openssl req -x509 \
+    -newkey rsa \
+    -sha384 \
+    -keyout server.key \
+    -out server.crt \
+    -days 365 \
+    -nodes \
+    -subj "/CN=${CN}"
 }
 function ssl.cert.ECDSA-SHA256() {
   [[ $# -ne 1 ]] && { echo "usage: $0 <domain-name>"; return 1; }
   local CN="$1"
   openssl req -x509 \
-  -newkey ec \
-  -pkeyopt ec_paramgen_curve:P-256 \
-  -keyout server.key \
-  -out server.crt \
-  -sha256 \
-  -days 365 \
-  -nodes \
-  -subj "/CN=${CN}"
+    -newkey ec \
+    -pkeyopt ec_paramgen_curve:P-256 \
+    -keyout server.key \
+    -out server.crt \
+    -sha256 \
+    -days 365 \
+    -nodes \
+    -subj "/CN=${CN}"
 }
 function ssl.cert.ECDSA-SHA384() {
   [[ $# -ne 1 ]] && { echo "usage: $0 <domain-name>"; return 1; }
   local CN="$1"
   openssl req -x509 \
-  -newkey ec \
-  -pkeyopt ec_paramgen_curve:P-256 \
-  -keyout server.key \
-  -out server.crt \
-  -sha384 \
-  -days 365 \
-  -nodes \
-  -subj "/CN=${CN}"
+    -newkey ec \
+    -pkeyopt ec_paramgen_curve:P-256 \
+    -keyout server.key \
+    -out server.crt \
+    -sha384 \
+    -days 365 \
+    -nodes \
+    -subj "/CN=${CN}"
 }
 
 ###############################################################################
