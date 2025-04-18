82a28aa26a35b8f1e83a70055b44436096ca73f5
oswystan <oswystan@126.com>
2025-04-18 10:20:03 +0800

    add ssl.cert.*

--- scripts/env/ssl.sh
+++ scripts/env/ssl.sh
@@ -7,10 +7,47 @@
 ##
 ###############################################################################
 
-alias crt.cat='openssl x509 -noout -text -in '
-alias cert.cat='openssl x509 -noout -text -in '
-alias csr.cat='openssl req -noout -text -in '
-alias key.rsa.cat='openssl rsa -noout -text -in '
-alias key.ec.cat='openssl ec -noout -text -in '
+alias ssl.crt.cat='openssl x509 -noout -text -in '
+alias ssl.cert.cat='openssl x509 -noout -text -in '
+alias ssl.csr.cat='openssl req -noout -text -in '
+alias ssl.key.rsa.cat='openssl rsa -noout -text -in '
+alias ssl.key.ec.cat='openssl ec -noout -text -in '
+
+function ssl.cert.RSA-SHA256() {
+  [[ $# -ne 1 ]] && { echo "usage: $0 <domain-name>"; return 1; }
+  local CN="$1"
+  openssl req -x509 -newkey rsa -sha256 -keyout server.key -out server.crt -days 365 -nodes -subj "/CN=${CN}"
+}
+function ssl.cert.RSA-SHA384() {
+  [[ $# -ne 1 ]] && { echo "usage: $0 <domain-name>"; return 1; }
+  local CN="$1"
+  openssl req -x509 -newkey rsa -sha384 -keyout server.key -out server.crt -days 365 -nodes -subj "/CN=${CN}"
+}
+function ssl.cert.ECDSA-SHA256() {
+  [[ $# -ne 1 ]] && { echo "usage: $0 <domain-name>"; return 1; }
+  local CN="$1"
+  openssl req -x509 \
+  -newkey ec \
+  -pkeyopt ec_paramgen_curve:P-256 \
+  -keyout server.key \
+  -out server.crt \
+  -sha256 \
+  -days 365 \
+  -nodes \
+  -subj "/CN=${CN}"
+}
+function ssl.cert.ECDSA-SHA384() {
+  [[ $# -ne 1 ]] && { echo "usage: $0 <domain-name>"; return 1; }
+  local CN="$1"
+  openssl req -x509 \
+  -newkey ec \
+  -pkeyopt ec_paramgen_curve:P-256 \
+  -keyout server.key \
+  -out server.crt \
+  -sha384 \
+  -days 365 \
+  -nodes \
+  -subj "/CN=${CN}"
+}
 
 ###############################################################################
