5da278a307e3d1cd317e7f6ae58391fdde9f6785
oswystan <oswystan@126.com>
2025-06-11 14:07:30 +0800

    github.pubkey.push works

--- scripts/github.sh
+++ scripts/github.sh
@@ -10,26 +10,40 @@
 ###########################################################################
 
 github.repo.ls() {
-    curl -s -X GET "https://api.github.com/users/oswystan/repos?per_page=200"|jq -r '.[].name'
+  curl -s -X GET "https://api.github.com/users/oswystan/repos?per_page=200" | jq -r '.[].name'
 }
 
 github.repo.clone() {
-    echo "===> $1"
-    git clone --mirror https://github.com/oswystan/$1 repo/$1.git
-    [[ $? -ne 0 ]] && exit 1
+  echo "===> $1"
+  git clone --mirror https://github.com/oswystan/$1 repo/$1.git
+  [[ $? -ne 0 ]] && exit 1
 }
 
 github.repo.backup() {
-    repos=$(github.repo.ls);
-    for one in ${repos}
-    do
-        github.repo.clone $one
-    done
+  repos=$(github.repo.ls)
+  for one in ${repos}; do
+    github.repo.clone $one
+  done
 }
 
 github.check() {
-    jq=$(which jq)
-    [[ -z ${jq} ]] && echo "NO jq found" && exit 1;
+  jq=$(which jq)
+  [[ -z ${jq} ]] && echo "NO jq found" && exit 1
+}
+
+github.pubkey.push() {
+  # 1. create a token on github settings: https://github.com/settings/tokens
+  # 2. export GITHUB_TOKEN="your-github-token"
+  # 3. create keypair by: yes | ssh-keygen -t ed25519
+  local GITHUB_API="https://api.github.com/user/keys"
+  local PUBKEY_CONTENT="$(cat "$HOME/.ssh/id_ed25519.pub")"
+  local KEY_TITLE="$(hostname)-$(basename "id_ed25519.pub")"
+
+  curl -s -w "%{http_code}" \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    -H "Accept: application/vnd.github+json" \
+    -X POST "$GITHUB_API" \
+    -d "{\"title\":\"$KEY_TITLE\",\"key\":\"$PUBKEY_CONTENT\"}"
 }
 
 ###########################################################################
--- scripts/install.ubuntu.sh
+++ scripts/install.ubuntu.sh
@@ -1,34 +1,48 @@
 #!/bin/bash
 
+set -e
+
 sudo apt-get update
 
-install_basic()
-{
-    sudo apt-get install -y vim cscope ctags gcc g++
-    sudo apt-get install -y git subversion sloccount tmux jq
-    sudo apt-get install -y manpages-posix-dev
-    sudo apt-get install -y gnome-tweak-tool #unity-tweak-tool
-    sudo apt-get install -y meld
+install_basic() {
+  sudo apt-get install -y vim cscope ctags gcc g++
+  sudo apt-get install -y git subversion sloccount tmux jq zip unzip
+  sudo apt-get install -y manpages-posix-dev
+  # sudo apt-get install -y gnome-tweak-tool #unity-tweak-tool
 }
 
 dl_pkg() {
-    wget http://dl2.xmind.cn/xmind-8-update5-linux.zip
+  wget http://dl2.xmind.cn/xmind-8-update5-linux.zip
 }
 
 install_cloc() {
-    sudo wget https://github.com/AlDanial/cloc/releases/download/v1.76/cloc-1.76.pl -O /usr/bin/cloc && sudo chmod +x /usr/bin/cloc
+  sudo wget https://github.com/AlDanial/cloc/releases/download/v1.76/cloc-1.76.pl -O /usr/bin/cloc && sudo chmod +x /usr/bin/cloc
 }
 
 install_android() {
-    sudo apt install -y android-platform-tools-base
+  sudo apt install -y android-platform-tools-base
 }
 
+install_nvim() {
+  local name="nvim-linux-x86_64" #nvim-linux-arm64
+  wget "https://github.com/neovim/neovim/releases/download/stable/${name}.tar.gz"
+  tar xvf "${name}.tar.gz"
+  sudo mv ${name} /opt/nvim
+  sudo ln -s /opt/nvim/bin/nvim /usr/local/bin/
+}
+
+install_node() {
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+  source "$HOME/.zshrc"
+  nvm install --lts
+}
 
 #####################################
 ##
 ## main
 ##
 #####################################
-install_basic
+# install_basic
 #install_cloc
 #dl_pkg
+install_nvim
