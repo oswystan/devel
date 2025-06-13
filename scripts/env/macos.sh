99d7a8d26345f127684e043fd8b7c48635f4cda3
oswystan <oswystan@126.com>
2025-06-13 09:43:44 +0800

    add macos.sh

---
+++ scripts/env/macos.sh
@@ -0,0 +1,17 @@
+###############################################################################
+##
+##       filename: macos.sh
+##    description:
+##        created: 2025/06/13
+##         author: ticktechman
+##
+###############################################################################
+
+function macos.bundleid() {
+  for one in "$@"; do
+    local bid="$(/usr/libexec/PlistBuddy -c 'Print CFBundleIdentifier' "$one/Contents/Info.plist")"
+    printf "%-60s : $bid\n" "$one"
+  done
+}
+
+###############################################################################
