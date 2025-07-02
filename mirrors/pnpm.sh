673378a53ad2aa21551980108ee848cbd1cfc8d5
oswystan <oswystan@126.com>
2025-07-02 10:29:51 +0800

    pnpm mirror

---
+++ mirrors/pnpm.sh
@@ -0,0 +1,8 @@
+## get current registry
+pnpm get registry
+
+## set registry to taobao mirror
+pnpm config set registry https://registry.npmmirror.com
+
+## reset to original npm site
+pnpm config set registry https://registry.npmjs.org/
